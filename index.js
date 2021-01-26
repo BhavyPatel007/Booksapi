a12aa012d39345fd6c94fc2074e41a96f9ec703e
BHAVY <bhavysoliya007@gmail.com>
2021-01-26 11:11:04 +0530

    hay

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-mongoose.connect("mongodb+srv://bhavy:Bhavy1225@cluster0.guwjf.mongodb.net/books?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect('mongodb+srv://bhavy:bhavy1255@cluster0.guwjf.mongodb.net/student?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use("/api",route);
