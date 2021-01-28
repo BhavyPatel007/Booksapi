275813296cc807ca297fe5b7e63b31c8acc6d2fd
BHAVY <bhavysoliya007@gmail.com>
2021-01-28 16:20:08 +0530

    post

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
+const bodyparser = require('body-parser');
 
 mongoose.connect('mongodb+srv://bhavy:bhavy1225@cluster0.guwjf.mongodb.net/student?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
+        app.use(bodyparser.urlencoded({extended:true}));
+        app.use(express.json());
         app.use("/api",route);
         app.listen(process.env.PORT || 3000,()=>{
             console.log("server started...!!!");
--- route.js
+++ route.js
@@ -7,4 +7,22 @@ router.get('/books',async (req,res)=>{
     const book = await Book.find();
     res.send(book);
 });
+
+router.post("/books",async(req,res)=>{
+    const book = new Book({
+        name:req.body.name,
+        qty:req.body.qty
+    });
+    await book.save();
+    res.send(book);
+});
+
+router.delete("/books/:id",async(req,res)=>{
+    try{
+        await Book.deleteOne({_id:req.params.id});
+        res.send("deleted");
+    }catch(error){
+        res.status(404).send({error:"book is not found"});
+    }
+});
 module.exports = router;
