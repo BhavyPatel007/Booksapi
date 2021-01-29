a2fc6160ad85e764b7be5b5e5c9c997639292d39
BHAVY <bhavysoliya007@gmail.com>
2021-01-29 16:28:15 +0530

    deleted

--- route.js
+++ route.js
@@ -3,39 +3,39 @@ const { model } = require('mongoose');
 const Book = require('./model/book');
 const router = express.Router();
 
-router.get('/books',async (req,res)=>{
+router.get('/books', async (req, res) => {
     const book = await Book.find();
     res.send(book);
 });
 
-router.post('/books',async(req,res)=>{
+router.post('/books', async (req, res) => {
     const book = new Book({
-        name:req.body.name,
-        qty:req.body.qty
+        name: req.body.name,
+        qty: req.body.qty
     });
     await book.save();
     res.send(book);
 });
 
-router.patch("/books/:id",async(req,res)=>{
-    try{
-        const book = await Book.findOne({_id:req.params.id});
+router.patch("/books/:id", async (req, res) => {
+    try {
+        const book = await Book.findOne({ _id: req.params.id });
         book.name = req.body.name;
-
+        book.qty = res.body.qty;
         await book.save();
         res.send(book);
-    }catch(error){
+    } catch (error) {
         res.send(error);
     }
 });
 
 
-router.delete('/books/:id',async(req,res)=>{
-    try{
-        await Book.deleteOne({_id:req.params.id});
+router.delete('/books/:id', async (req, res) => {
+    try {
+        await Book.deleteOne({ _id: req.params.id });
         res.send("deleted");
-    }catch(error){
-        res.status(404).send({error:"book is not found"});
+    } catch (error) {
+        res.status(404).send({ error: "book is not found" });
     }
 });
 module.exports = router;
